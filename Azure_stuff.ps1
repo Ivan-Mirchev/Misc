aa1a699b8be0e9af39f0178b5c4292779ec1cc23
Ivan-Mirchev <ivan.mirchev@gmail.com>
2018-10-29 21:07:10 +0200

    Add files via upload

---
+++ Azure_stuff.ps1
@@ -0,0 +1,43 @@
+﻿break
+
+# Login to Azure
+Login-AzureRmAccount 
+
+# Create Resource Group
+
+$RGName = 'ITGM-AzurePowerShell'
+$Location = 'North Europe'
+
+New-AzureRmResourceGroup -Name $RGName -Location $Location -Verbose
+
+# Create Storage Account using splatting 
+# Storage account name must be between 3 and 24 characters in length and use numbers and lower-case letters only.
+
+$StorageAccountName = 'itglazurepowershell'
+$StorageAccountType = 'Standard_LRS' # Validate Set
+
+$storageAccountProps = @{
+    Name = $StorageAccountName
+    ResourceGroupName = $RGName
+    Type = 'Standard_LRS'
+    Location = $Location
+}
+
+New-AzureRmStorageAccount @storageAccountProps -Verbose
+
+
+
+
+
+#remove all
+
+# Remove Resource Group
+Get-AzureRmResourceGroup -Name $RGName
+Remove-AzureRmResourceGroup -Name $RGName -Verbose
+
+# Remove Storage Account
+Get-AzureRmStorageAccount -Name $StorageAccountName -ResourceGroupName $RGName 
+Remove-AzureRmStorageAccount -Name $StorageAccountName -ResourceGroupName $RGName
+
+
+
