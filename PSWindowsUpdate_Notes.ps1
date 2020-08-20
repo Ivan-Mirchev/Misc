abd5c36c7c5efaac27f37c94d01b8b442caaf8e1
Ivan Mirchev - TBS <TBS.imirchev@mon.bg>
2020-08-20 13:52:46 +0300

    Update

---
+++ PSWindowsUpdate_Notes.ps1
@@ -0,0 +1,14 @@
+Get-WUSettings
+
+<# 
+    Default Settings 
+
+    ComputerName         : SRV-MGMT-FLE
+    NoAutoUpdate         : 0
+    AUOptions            : 3 - Notify before installation
+    ScheduledInstallDay  : 0 - Every Day
+    ScheduledInstallTime : 3
+
+#>
+
+Set-WUSettings
